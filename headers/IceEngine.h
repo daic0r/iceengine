82e06b26de35e5883ae8e405087219ec0d6cab85
Matthias Grün <gruen.matthias@gmx.net>
2022-10-16 23:07:10 +0200

    Fixed massive bugs (_DEBUG symbol was defined, __declspec(xxx) was defined to be nothing)

--- headers/IceEngine.h
+++ headers/IceEngine.h
@@ -5,17 +5,3 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #define WINDOWS_SYSTEM
 #endif
-
-#ifdef WINDOWS_SYSTEM
-
-#ifdef ICEENGINE_DLL_EXPORTS
-#define __declspec(dllexport)
-#else
-#define __declspec(dllimport)
-#endif
-
-#else
-
-#define
-
-#endif
